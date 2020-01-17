6481a9410482dd354d5b2cd558ee47fffac7fe2a
birdincee <incee.jan@bncc.ac.th>
2020-01-17 21:16:10 +0700

    แก้ไขหน้า Editeven ให้เป็นเหมือนเดิม

--- editEventTitle.php
+++ editEventTitle.php
@@ -1,35 +1,21 @@
 <?php
 
 require_once('connect.php');
-$mysqli = new mysqli("localhost","root","root","calender");
-if ($mysqli -> connect_errno) {
-  echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
-  exit();
-}
 if (isset($_POST['delete']) && isset($_POST['id'])){
 	
 	$id = $_POST['id'];
 	
-	$strSQL = "SELECT * FROM account WHERE userLevel = 'admin' ";
-	$objQuery = mysqli_query($bdd,$strSQL);
-	$objResult = mysqli_fetch_array($objQuery);
-	if($objResult)
-	{
-			$sql = "DELETE FROM events WHERE id = $id";
-			$query = $bdd->prepare( $sql );
-			if ($query == false) {
-		 	print_r($bdd->errorInfo());
-		 	die ('Erreur prepare');
-			}
-			$res = $query->execute();
-			if ($res == false) {
-		 	print_r($query->errorInfo());
-			die ('Erreur execute');
-	}		 
-	else{
-		echo '<script>alert("กรุณาติดต่อผู้ดูแลระบบ")window.location = "calendar.php";</script>';
-		}
-}
+	$sql = "DELETE FROM events WHERE id = $id";
+	$query = $bdd->prepare( $sql );
+	if ($query == false) {
+	 print_r($bdd->errorInfo());
+	 die ('Erreur prepare');
+	}
+	$res = $query->execute();
+	if ($res == false) {
+	 print_r($query->errorInfo());
+	 die ('Erreur execute');
+	}
 	
 }elseif (isset($_POST['title']) && isset($_POST['color']) && isset($_POST['id'])){
 	
@@ -51,5 +37,5 @@ if (isset($_POST['delete']) && isset($_POST['id'])){
 	}
 
 }
-
-
+echo '<script>alert("เรียบร้อยแล้ว");window.location = "calendar.php";</script>';
+?>
